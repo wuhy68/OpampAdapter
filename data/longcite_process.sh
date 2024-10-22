c8af707f519102ccad5c9b12ba3886bc873cae27
Rui Ming <108508852+Atumyalimu@users.noreply.github.com>
2024-10-22 22:23:59 +0800

    Add files via upload

---
+++ data/longcite_process.sh
@@ -0,0 +1,4 @@
+python longcite_process.py \
+    --data_name_or_path '/root/autodl-tmp/LongCite-45k' \
+    --output_dir '/root/autodl-tmp/LongCite-45k' \
+    --num_proc 16
---
+++ readme.md
@@ -0,0 +1,10 @@
+## Data Processing
+
+
+
+### How to get the processed LongCite datasets
+
+``` shell
+sh longcite_process.sh
+```
+
