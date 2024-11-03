dd4d0c8a399fd0c15a7cafc5de595f90dead4e5a
Atumyalimu <987156185@qq.com>
2024-11-03 17:08:37 +0800

    RGB_evaluation

---
+++ evaluation/RGB/run_rgb.sh
@@ -0,0 +1,9 @@
+# don't need to change modelname
+
+python evalue.py \
+--dataset en_refine \
+--modelname Llama-2 \
+--temp 0.2 \
+--noise_rate 0.6 \
+--plm ~/autodl-tmp/llama_8B \
+--passage_num 5
